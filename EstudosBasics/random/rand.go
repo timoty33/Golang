04688ae7bff0b9c454bbf9c56bd0b849fa0fd803
timoty33 <timotyduo@gmail.com>
2025-07-01 10:28:41 -0300

    CURSO

---
+++ EstudosBasics/random/rand.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	var numEsp int
+	var numNor int
+
+	for i := 0; i < 10; i++ {
+
+		num := rand.Intn(10) + 1
+
+		if num <= 5 {
+			fmt.Printf("\033[1;32;40mO seu número é especial: %v\n\033[0;0;0m", num)
+			numEsp++
+		} else {
+			fmt.Printf("\033[1;31;40mO seu número é totalmente normal: %v\n\033[0;0;0m", num)
+			numNor++
+		}
+
+	}
+
+	fmt.Println("Números especiais:", numEsp)
+	fmt.Println("Números normais:", numNor)
+
+}
